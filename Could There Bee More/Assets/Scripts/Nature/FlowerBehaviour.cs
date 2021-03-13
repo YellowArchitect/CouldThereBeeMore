66c14ca47092068caa39bde7c61dc4669c5f10bd
Ben Schmidt <54752448+YellowArchitect@users.noreply.github.com>
2021-03-13 01:06:35 -0500

    Flower stuff

---
+++ Could There Bee More/Assets/Scripts/Nature/FlowerBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowerBehaviour : MonoBehaviour
+{
+
+    public Color flowerColor;
+
+    bool collected;
+
+    SpriteRenderer sr;
+
+    void Start()
+    {
+        collected = false;
+        sr = GetComponent<SpriteRenderer>();
+        update_color();
+    }
+
+
+    public bool is_collectable()
+    {
+        return !collected;
+    }
+
+    public Color collect_pollen()
+    {
+        print("flower collected");
+        collected = true;
+        return flowerColor;
+    }
+
+    // Call to update the flower color
+    public void update_color()
+    {
+        sr.color = flowerColor;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Could There Bee More/Assets/Scripts/Player/PlayerController.cs
+++ Could There Bee More/Assets/Scripts/Player/PlayerController.cs
@@ -8,7 +8,8 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 0.05f;
     public float turnSpeed = 3f;
 
-    Rigidbody2D rb;
+    Rigidbody2D rigidbody2d;
+    Collider2D collider2d;
 
     float horizontal;
     float vertical;
@@ -16,7 +17,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -24,16 +25,21 @@ public class PlayerController : MonoBehaviour
     {
         horizontal = turnSpeed * Input.GetAxis("Horizontal");
         vertical = moveSpeed * Input.GetAxis("Vertical");
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            print("pressed shift");
+        }
     }
 
     private void FixedUpdate()
     {
-        Vector2 position = rb.position;
-        float rotation = rb.rotation;
+        Vector2 position = rigidbody2d.position;
+        float rotation = rigidbody2d.rotation;
         position.x -= vertical * Mathf.Sin((rotation/180)*Mathf.PI);
         position.y += vertical * Mathf.Cos((rotation / 180) * Mathf.PI);
         rotation -= horizontal;
-        rb.MovePosition(position);
-        rb.MoveRotation(rotation);
+        rigidbody2d.MovePosition(position);
+        rigidbody2d.MoveRotation(rotation);
     }
 }
