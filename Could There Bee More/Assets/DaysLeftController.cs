121bd40fe1fd10f636f094adb75203a15a7d5a40
zachtl13 <iamzachlong@gmail.com>
2021-03-14 12:03:18 -0400

    Added days left UI
    20 days...........

---
+++ Could There Bee More/Assets/DaysLeftController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DaysLeftController : MonoBehaviour
+{
+    private PlayerController player;
+    private Text dayText;
+
+    private void Start()
+    {
+        GameObject _playerGO = GameObject.FindGameObjectWithTag("Player");
+        if (_playerGO)
+        {
+            player = _playerGO.GetComponent<PlayerController>();
+        }
+
+        dayText = GetComponentInChildren<Text>();
+    }
+
+    private void Update()
+    {
+        if (player && dayText)
+        {
+            dayText.text = $"{player.get_days_left()}\ndays left";
+        }
+    }
+}
